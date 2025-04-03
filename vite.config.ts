64de998fafa3953ada2cbc9dddf985e8c651f3b3
niyang <niyang@sundear.com>
2025-04-03 18:23:57 +0800

    临时打包
    已经烂了 垃圾es6

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+
+function replaceCrossorigin() {
+    return {
+        name: 'replace-crossorigin',
+        transformIndexHtml(html: string) {
+            // console.log('html', html)
+            return html.replace(/crossorigin /g, '')
+        },
+    }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: 'https://gitee.com/qingyun-cottage/resource/raw/main/single/th-summary',
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        // legacy({
+        //     targets: [], // 根据需要调整目标浏览器
+        //     polyfills: true, // 自动注入必要的 Polyfill
+        // }),
+        // replaceCrossorigin(),
+    ],
     resolve: {
         alias: {
             '@': '/src',
@@ -15,4 +33,7 @@ export default defineConfig({
         port: 8066, // 指定启动端口
         open: false, //启动后是否自动打开浏览器
     },
+    // build: {
+    //     target: 'es2015',
+    // },
 })
